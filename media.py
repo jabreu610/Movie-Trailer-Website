3a7cea3b24cf0511a23b7996ea9cfd06a209a5a1
Jordan Abreu <abreu@apple.com>
2017-07-27 01:03:11 -0700

    feature: Creates media.py which contains Movie class adds empty gitignore file

---
+++ media.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+class Movie(object):
+    """Class that describes the components of a Movie"""
+    # Define constructor method
+    def __init__(self, title, poster_image_url, trailer_youtube_url):
+        super(Movie, self).__init__()
+        self.title = title
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
