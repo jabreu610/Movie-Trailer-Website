c132bd1e11e2bc0c49ca5444f6959fa152600e48
Jordan Abreu <abreu@apple.com>
2017-07-27 02:05:29 -0700

    task: adds comments and runs autopep8 on python files

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,11 +3,17 @@ from fresh_tomatoes import *
 from media import Movie
 import json
 
+# Loads json and creates a list of Movie objects
+
+
 def main():
     with open('favorite_movies.json') as content:
         data = json.load(content)
     movies = map(create_movie, data)
     open_movies_page(movies)
+
+# Takes movie dictionary as parameter and returns Movie object
+
 
 def create_movie(movie_dict):
     return Movie(movie_dict.get('title'), movie_dict.get('poster_image_url'), movie_dict.get('trailer_youtube_url'))
--- media.py
+++ media.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
+
 class Movie(object):
     """Class that describes the components of a Movie"""
     # Define constructor method
+
     def __init__(self, title, poster_image_url, trailer_youtube_url):
         super(Movie, self).__init__()
         self.title = title
